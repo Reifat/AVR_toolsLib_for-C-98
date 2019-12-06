b00c72e9627eca725c179c582e752d777b4251ec
Reyfat <NewFatalistik@yandex.ru>
2019-12-06 06:57:27 +0300

    stable version of initialization and register bit setting methods

---
+++ registerSet/registerBitInit.h
@@ -0,0 +1,57 @@
+/*
+Example,
+Initialize bits of the register:
+	registerSet<typeRegister> objRegister =
+	RBI::Init<typeRegister,&pointerToRegister>(1)(1)(1)(0)(1)(1)(1)(0);
+Example:
+Setting bits of the register:
+	objPORTB = RBI::Init<typeRegister>(1)(0)(1)(0)(1)(0)(0)(1);
+*/
+
+namespace RBI // namespace RegisterBitInitialization
+{
+	reg8_t* PNULL;
+
+	template<typename _Ty, _Ty * *P>
+	class generateListBits
+	{
+	public:
+		generateListBits() :_ListBist((_Ty)0)
+		{
+			_SizeRegister = (sizeof(_Ty) * 8) - 1;
+			_MyPos = _SizeRegister;
+		}
+		~generateListBits() {}
+		generateListBits& operator()(_Ty _Val)
+		{
+			_Val ? _ListBist |= ((_Ty)1 << _MyPos) : _ListBist &= ~((_Ty)1 << _MyPos);
+			_MyPos--;
+			return *this;
+		}
+		operator registerSet<_Ty>()
+		{
+			if (*P)
+			{
+				**P = _ListBist;
+				return registerSet<_Ty>(*P);
+			}
+			else
+				return registerSet<_Ty>(&_ListBist);
+		}
+	private:
+		_Ty _ListBist;
+		_Ty _MyPos;
+		_Ty _SizeRegister;
+	};
+
+	template<typename _Ty, _Ty * *P>
+	static generateListBits<_Ty, P> Init(_Ty _Val)
+	{
+		return generateListBits<_Ty, P>()(_Val);
+	}
+	template<typename _Ty>
+	generateListBits<_Ty, &PNULL> Init(_Ty _Val)
+	{
+		return generateListBits<_Ty, &PNULL>()(_Val);
+	}
+}
